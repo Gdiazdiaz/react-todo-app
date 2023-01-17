f6831b7a60bcf57a3d58dbc9606583181eef7a8c
Glenda Diaz <glendadiazah@outlook.com>
2023-01-17 12:04:37 -0600

    add style.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './components/TodoContainer';
+import 'style.css'
 // component file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
