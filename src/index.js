dac10803dc54d67acd2eff22fed5737d55de0c9f
Glenda Diaz <glendadiazah@outlook.com>
2023-01-17 12:10:34 -0600

    fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './components/TodoContainer';
-import 'style.css'
+import 'index.css';
 // component file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
